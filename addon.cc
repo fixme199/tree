ce461dac24deee929656b2f2fb22b558d71bd83b
fixme199 <fixme199@gmail.com>
2019-06-20 23:48:11 +0900

    recursive_directory_iterator to object

--- addon.cc
+++ addon.cc
@@ -4,11 +4,7 @@
 #include <algorithm>
 #include <filesystem>
 
-#include "rapidjson/document.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
-
-using namespace rapidjson;
+using namespace v8;
 
 namespace fs = std::filesystem;
 
@@ -25,40 +21,40 @@ NAN_METHOD(Method)
     return;
   }
 
-  v8::Isolate *isolate = info.GetIsolate();
-  v8::String::Utf8Value arg0(isolate, info[0]);
+  Isolate *isolate = info.GetIsolate();
+  String::Utf8Value arg0(isolate, info[0]);
   std::string rootPath(*arg0);
   std::cout << rootPath << std::endl;
-  // Document document;
-  // document.SetObject();
-  // Document::AllocatorType &allocator = document.GetAllocator();
-  //
-  // document.AddMember("Name", "XYZ", allocator);
-  // document.AddMember("Rollnumer", 2, allocator);
-  // Document child;
-  // child.SetObject();
-  // document.AddMember("child", child, allocator);
-  //
-  // StringBuffer buffer;
-  // Writer<StringBuffer> writer(buffer);
-  // document.Accept(writer);
-  //
-  // std::cout << buffer.GetString() << std::endl;
+
+  Local<Object> document = Object::New(isolate);
+
+  auto key_path = String::NewFromUtf8(isolate, "path");
+  auto key_parent_path = String::NewFromUtf8(isolate, "parent_path");
+  auto key_filename = String::NewFromUtf8(isolate, "filename");
+  auto key_extension = String::NewFromUtf8(isolate, "extension");
+  auto key_is_directory = String::NewFromUtf8(isolate, "is_directory");
 
   std::error_code ec;
   for (const fs::directory_entry &x : fs::recursive_directory_iterator(rootPath,
                                                                        fs::directory_options::skip_permission_denied, ec))
   {
-    std::cout << "--- start ---" << std::endl;
-    std::cout << "filename : " << x.path().filename() << std::endl;
-    std::cout << "parent_path : " << x.path().parent_path() << std::endl;
-    std::cout << "stem : " << x.path().stem() << std::endl;
-    std::cout << "extension : " << x.path().extension() << std::endl;
-    std::cout << "path : " << x.path() << std::endl;
-    std::cout << "is_directory : " << x.is_directory() << std::endl;
-    std::cout << "--- end ---" << std::endl;
+    auto path = String::NewFromUtf8(isolate, x.path().string<char>().c_str());
+    auto parent_path = String::NewFromUtf8(isolate, x.path().parent_path().string<char>().c_str());
+    auto filename = String::NewFromUtf8(isolate, x.path().filename().string<char>().c_str());
+    auto extension = String::NewFromUtf8(isolate, x.path().extension().string<char>().c_str());
+    auto is_directory = Boolean::New(isolate, x.is_directory());
+
+    Local<Object> child = Object::New(isolate);
+    child->Set(key_path, path);
+    child->Set(key_parent_path, parent_path);
+    child->Set(key_filename, filename);
+    child->Set(key_extension, extension);
+    child->Set(key_is_directory, is_directory);
+
+    document->Set(path, child);
   }
-  info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
+
+  info.GetReturnValue().Set(document);
 }
 
 NAN_MODULE_INIT(init)
